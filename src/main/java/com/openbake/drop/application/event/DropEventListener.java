0724eb6cad1ed230c1664126d3e2aed439f5ce49
deukr <deukryeongoh@gmail.com>
2026-07-25 11:27:16 +0900

    fix: resolve Merge Conflict Add Propagation Options With Transactional

--- src/main/java/com/openbake/drop/application/event/DropEventListener.java
+++ src/main/java/com/openbake/drop/application/event/DropEventListener.java
@@ -6,6 +6,7 @@ import com.openbake.drop.domain.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -20,7 +21,7 @@ public class DropEventListener {
     private final DropInventoryRepository dropInventoryRepository;
     private final DropEntryRepository dropEntryRepository;
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handlePaymentCompleted(PaymentCompletedEvent event){
         // 결제 완료 시 드롭 대기열 재입장 불가 (entryStatus Completed로 변경)
@@ -35,15 +36,15 @@ public class DropEventListener {
         });
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handlePaymentFailed(PaymentFailedEvent event){
         // 결제 실패 시 entry 상태 값 변경 후 재고 복구
         Optional<DropEntry> dropEntry = dropEntryRepository.findByDropIdAndMemberId(event.dropId(), event.memberId());
         dropEntry.ifPresent(entry -> {
-            if (entry.getEntryStatus() != EntryStatus.RESERVED) {
-                return ;
-            }
+            if (entry.getEntryStatus() != EntryStatus.RESERVED)
+                return;
+
             entry.failPaymentOrOrder();
             dropInventoryRepository.findByDropId(entry.getDropId()).increaseStock(event.quantity());
         });

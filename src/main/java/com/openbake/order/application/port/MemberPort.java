ed5389a274c7d9afa49d19b8571d41afb478ea52
sejong <hhh3915@gmail.com>
2026-08-10 09:24:00 +0900

    fix: MemberPort 인터페이스에서 불필요한 @PathVariable 제거

--- src/main/java/com/openbake/order/application/port/MemberPort.java
+++ src/main/java/com/openbake/order/application/port/MemberPort.java
@@ -2,8 +2,7 @@ package com.openbake.order.application.port;
 
 import com.openbake.common.response.ApiResponse;
 import com.openbake.order.application.port.dto.MemberInfo;
-import org.springframework.web.bind.annotation.PathVariable;
 
 public interface MemberPort {
-    ApiResponse<MemberInfo> getMember(@PathVariable Long memberId);
+    ApiResponse<MemberInfo> getMember(Long memberId);
 }
